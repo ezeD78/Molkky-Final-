1a8cb47882f9a69e77df9a00da7cc35835a9c327
Ezequiel Gomes <ezequiel7897@gmail.com>
2024-04-25 11:27:26 +0200

    Initial Commit

---
+++ Molkky/MolkkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MolkkyApp.swift
+//  Molkky
+//
+//  Created by Ezequiel Gomes on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MolkkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
